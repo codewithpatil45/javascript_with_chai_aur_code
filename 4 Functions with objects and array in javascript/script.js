be3388838da567b4eb9c7350e7d9b3841a128894
Patil <codewithpatil45@gmail.com>
2025-09-14 11:20:02 +0530

    Add user object and welcomeMessage function
    Commented out unused code and introduced a user object with a welcomeMessage method to demonstrate object methods and scope. Added tempCodeRunnerFile.js to test changing the username property.

--- 4 Functions with objects and array in javascript/script.js
+++ 4 Functions with objects and array in javascript/script.js
@@ -1,15 +1,31 @@
 console.log("Patil");
 //Functions with objects and array in javascript 
-if {true}{
+// if {true}{
     
-    let a =50
-    const b =85
-   3.
+//     let a =50
+//     const b =85
+//    3.
    
     
     
-}
+// }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//Scope level and mini hoisting in javascript
+
+//arrow function 
+
+const user = {
+  username: "Atharva",
+  price: 999,
+  welcomeMessage: function () {
+    console.log(`Welcome To Our Website, ${this.username}`);
+  }
+};
+
+// user.welcomeMessage(); // Welcome To Our Website, Atharva
+// user.username = "Patil"
+// username.welcomeMessage(); 
